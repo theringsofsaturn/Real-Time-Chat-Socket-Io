817e647fa91fb7f7bcbf6bcfe9c01d49a59552ee
theringsofsaturn <emiliankasemi@gmail.com>
2021-12-09 00:33:05 +0100

    router added

--- server/index.js
+++ server/index.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
+const router = require("./router");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+app.use(router);
+
 server.listen(PORT, () => {
-  console.log(`listening on port ${3000}`);
+  console.log(`listening on port ${3001}`);
 });
