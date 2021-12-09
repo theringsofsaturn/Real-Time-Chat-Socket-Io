c886de45ed2c0d5a22a564c65a85ecf5d7f34eee
theringsofsaturn <emiliankasemi@gmail.com>
2021-12-09 16:06:06 +0100

    handling disconnection

--- server/index.js
+++ server/index.js
@@ -19,8 +19,14 @@ io.on("connection", (socket) => {
   // When a user connects to the server through socket.io (browser) this function is called and the socket is passed in as an argument to the function. The socket will be connected as client side socket.);
   console.log("New client connected");
 
-  socket.on("join", ({ name, room }) => {
+  socket.on("join", ({ name, room }, callback) => {
     console.log(name, room);
+    // With a callback we can trigger some response immediately after the socket.on event ("join") has been emitted. Here we can do some error handling.
+    // We can have acces to this function here as a third parameter in the socket.emit in the client (Chat.js)
+    // const error = true;
+    // if (error) {
+    //   return callback({error: "Error"});
+    // }
   });
   // In this case, socket.on not io because we have a specific socket (the one as parameter) that disconnects.
   socket.on("disconnect", () => console.log("Client disconnected"));
